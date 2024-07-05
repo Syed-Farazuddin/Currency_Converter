fd00a5c659342f76236d5b5a6818ea27259c9c2f
Syed Faraz <syedfarazuddin73@gmail.com>
2024-07-05 14:50:45 +0530

    completed

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -1,52 +1,105 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter/widgets.dart';
 
-class CurrencyConverterPage extends StatelessWidget {
+class CurrencyConverterPage extends StatefulWidget {
   const CurrencyConverterPage({super.key});
 
   @override
+  State<CurrencyConverterPage> createState() =>
+      _CurrencyConverterMaterialPage();
+}
+
+class _CurrencyConverterMaterialPage extends State<CurrencyConverterPage> {
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.blueGrey,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '0',
-              style: TextStyle(
-                fontSize: 45,
-                fontWeight: FontWeight.w700,
-                color: Colors.white,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: TextField(
-                style: TextStyle(
-                  color: Colors.black,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          title: const Text(
+            "Currency Converter",
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'INR ${result.toStringAsFixed(2)}',
+                style: const TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white,
                 ),
-                decoration: InputDecoration(
-                  hintText: "Enter the currency here",
-                  hintStyle: TextStyle(
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  controller: textEditingController,
+                  style: const TextStyle(
                     color: Colors.black,
                   ),
-                  prefixIcon: Icon(
-                    Icons.monetization_on,
+                  decoration: const InputDecoration(
+                    hintText: "Enter the currency here",
+                    hintStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    prefixIcon: Icon(
+                      Icons.monetization_on,
+                    ),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          width: 2.0,
+                          style: BorderStyle.solid,
+                          strokeAlign: BorderSide.strokeAlignInside),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    fillColor: Colors.white,
                   ),
-                  filled: true,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                        width: 2.0,
-                        style: BorderStyle.solid,
-                        strokeAlign: BorderSide.strokeAlignInside),
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                  ),
-                  fillColor: Colors.white,
+                  keyboardType: TextInputType.number,
                 ),
               ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextButton(
+                  onPressed: () {
+                    try {
+                      setState(() {
+                        result = double.parse(textEditingController.text) * 80;
+                      });
+                    } catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Please enter a valid number'),
+                        ),
+                      );
+                    }
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    // primary: Colors.white,
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    "Convert into INR",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
